c891087d7652f049e601c0c21b80ef2d5d10bfb2
ESSS Admin <admin@esss.ca>
2018-05-08 21:35:02 -0700

    Added motor implementation

--- rcdrivecamera.py
+++ rcdrivecamera.py
@@ -1,13 +1,15 @@
 # Import packages
 from imutils.video import VideoStream;
+import frameProcess as fp;
+from pololu_drv8835_rpi import motors, MAX_SPEED
 import imutils;
 import time;
 import datetime;
 import argparse;
 import cv2;
 
-# Running program for the rc car
-def drive(usePiCamera=True, resolution=(1648, 1232), fps=30):
+# Main running program for the car
+def drive(usePiCamera=True, resolution=(1648, 1232), fps=30, speedLimit=MAX_SPEED):
 	# Initialize the video stream and allow the camera sensor to warmup
 	isPiCamera=True;
 	vs = VideoStream(usePiCamera=usePiCamera, resolution=resolution, framerate=fps).start();
@@ -17,21 +19,28 @@ def drive(usePiCamera=True, resolution=(1648, 1232), fps=30):
 	fourcc = cv2.VideoWriter_fourcc(*'XVID');
 	out = cv2.VideoWriter('output.avi', fourcc, fps, resolution);
 
-	# Streaming the video
+	# Initialise motors
+	# Assume motor1 is longitudinal (forwards/backwards)
+	# Assume motor2 is lateral (turning or yaw)
+	motors.motor1.setSpeed(speedLimit);
+	motors.motor2.setSpeed(0);
+
+	# Event loop
 	while (True):
 		# Grabbing frame from the threaded video and resize it to 400 pixels
 		frame = vs.read();
 
-		# Doing image processing operations
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
-		frame = imutils.auto_canny(frame);
+		# Detect lanes
+		frame = fp.detectLanesFrame(frame);
 
-		# Draw the timestamp on the frame
-	# 	timestamp = datetime.datetime.now();
-	# 	ts = timestamp.strftime("%d %B %Y %I:%M:%S%p");
-	# 	print(str(ts));
-	# 	cv2.putText(frame, ts, (100, 100), cv2.FONT_HERSHEY_SIMPLEX,
-	# 1, (0, 0, 255), 1);
+		# Do some lane stuff
+
+		# # Draw the timestamp on the frame
+		# 	timestamp = datetime.datetime.now();
+		# 	ts = timestamp.strftime("%d %B %Y %I:%M:%S%p");
+		# 	print(str(ts));
+		# 	cv2.putText(frame, ts, (100, 100), cv2.FONT_HERSHEY_SIMPLEX,
+		# 1, (0, 0, 255), 1);
 
 		# Displaying frame
 		cv2.imshow("Frame", frame);
